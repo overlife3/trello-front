2ce76f551bca209ee12a505a233c1bfd03f11011
overlife3 <konsem04@mail.ru>
2026-09-09 11:21:22 +0300

    first commit

---
+++ src/app/layouts/private-layout/ui/profile.tsx
@@ -0,0 +1,22 @@
+import { useSession } from "@/entities/session";
+import { getAvatarUrl } from "@/entities/user";
+import Image from "next/image";
+
+export function Profile() {
+  const { currentSession } = useSession();
+
+  if (!currentSession) return null;
+
+  return (
+    <div className="flex gap-2 items-center justify-end">
+      <Image
+        width={32}
+        height={32}
+        className="w-8 h-8"
+        alt="avatar"
+        src={getAvatarUrl(currentSession.avatarId)}
+      />
+      <div className="text-lg">{currentSession.name}</div>
+    </div>
+  );
+}
---
+++ src/entities/user/ui/avatar-list.tsx
@@ -0,0 +1,26 @@
+import { getAvatarUrl } from "@/entities/user";
+import clsx from "clsx";
+import Image from "next/image";
+
+export function AvatarsList({
+  avatarsIds,
+  className,
+}: {
+  avatarsIds: string[];
+  className?: string;
+}) {
+  return (
+    <div className={clsx("flex", className)}>
+      {avatarsIds.map((id, key) => (
+        <Image
+          key={key}
+          alt="avatar"
+          width={32}
+          height={32}
+          className="-mr-2 last:mr-0 w-8 h-8"
+          src={getAvatarUrl(id)}
+        />
+      ))}
+    </div>
+  );
+}
